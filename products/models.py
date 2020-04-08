f0ef3b33b1fad530781e870623f4a04e9ca5edf7
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2020-04-09 03:23:19 +0500

    initial commit

---
+++ products/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Product(models.Model):
+
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    url = models.TextField()
+    icon = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/')
+    pub_date = models.DateTimeField()
+    votes_total = models.IntegerField(default=1)
+    
+    
+   
+
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+        
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+
+    def pub_date_year(self):
+        return self.pub_date.strftime('%b %e, %Y')        
